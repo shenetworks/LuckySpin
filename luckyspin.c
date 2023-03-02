ffdb577eb3414d817db0dc73e57628c10cd5e15e
shenetworks <91444360+shenetworks@users.noreply.github.com>
2023-03-02 14:46:46 -0600

    Add files via upload

--- luckyspin.c
+++ luckyspin.c
@@ -1,12 +1,12 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <time.h>
-// Expected result: buffer overflow should allow the attacker to get the unlimited food code
+// Expected result: buffer overflow should allow the attacker to get the unlimited food code by increasing their alloted spins 
 // 3 spins per day allowed unless you get a free spin
 int main() {
     
     srand(time(0));
-    int numberOfSpins = 0;
+    int numberOfSpins = 0; // numberOfSpins starts out at 0 and increases by 1 every valid spin
 
     printf("Welcome to Lucky Spin\n");
     printf("You get 3 chances per day to win yummy prizes\n");
@@ -25,7 +25,7 @@ int main() {
         // printf("numberOfSpins:%p\n", &numberOfSpins);
 
         if (spins == 1) {
-            numberOfSpins++;
+            numberOfSpins++; //this will increase the numberOfSpins by 1 every spin
             switch (spinNumber) {
                 case 1:
                     printf("You won 1 (one) free milkshake!\n");
@@ -55,24 +55,26 @@ int main() {
                     printf("CONGRATS! You won the jackpot! Unlimited food for life! Use code X72FG4S\n");
                     break;
                 case 10:
-                    numberOfSpins -= 1; // code for free spin
+                    numberOfSpins -= 1; // free spin will decrease numberOfSpins by 1
                     printf("You won a free spin!\n"); 
                     break;
                 default:
                     printf("Not a winner, better luck next time!\n");
             }
-        } else if (spins == 2) {
+        } else if (spins == 2) { // user chooses not to spin 
             printf("Come back later when you want to spin\n");
             continue; 
-        } else {
+        } else { // if the user input isnt a 1 or a 2 it will be deemed an invalid input
             printf("Invalid Entry\n");
         }
     }    
-    if (numberOfSpins >= 3) {
+    if (numberOfSpins == 3) { // once numberOfSpins reaches 3 program will end
         printf("You are out of spins, come back tomorrow to try again\n");
         // printf("Number of spins used: %i/3 \n", numberOfSpins );
 
-    }  else {
+    }  else if (numberOfSpins > 3) { //if numberOfSpins is greater than 3 it will throw and error and quit 
+        printf("Oops! There seems to be an error. Try again tomorrow.");
+    } else {
         printf("Invalid Entry");
     }
 }
